1c96a060022f3651bce8125b92c255e278f882d5
Sadik Bakiu <sbakiu@gmail.com>
2016-08-25 15:24:08 +0200

    Application config class

---
+++ simpleproject/src/main/java/com/allianz/simpleproject/service/ApplicationConfig.java
@@ -0,0 +1,9 @@
+package com.allianz.simpleproject.service;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("rest")
+public class ApplicationConfig extends Application{
+
+}
